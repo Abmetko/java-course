2319640a2f1f516d9d2f59b592f9c4baa2c4ad18
abmetko.a <abmetko.a@gmail.com>
2021-07-04 23:25:22 +0300

    init

--- src/main/java/com/patterns/factory/ScreenFactory.java
+++ src/main/java/com/patterns/factory/ScreenFactory.java
@@ -5,20 +5,11 @@ import com.enums.Screen;
 public class ScreenFactory {
 
     public BaseScreen getScreen(Screen type) {
-        BaseScreen screen;
-        switch (type) {
-            case SPLASH:
-                screen = new SplashScreen();
-                break;
-            case SIGN_IN:
-                screen = new LoginScreen();
-                break;
-            case MAIN:
-                screen = new MainScreen();
-                break;
-            default:
-                throw new IllegalArgumentException("screen name is incorrect:" + type);
-        }
-        return screen;
+        return switch (type) {
+            case SPLASH -> new SplashScreen();
+            case SIGN_IN -> new LoginScreen();
+            case MAIN -> new MainScreen();
+            default -> throw new IllegalArgumentException("screen name is incorrect:" + type);
+        };
     }
 }
