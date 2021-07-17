890d140b08364bb73076ae48fb16f70db1f0d188
abmetko.a <abmetko.a@gmail.com>
2021-07-17 23:31:27 +0300

    refactoring

--- src/main/java/com/patterns/strategy/TestStrategy.java
+++ src/main/java/com/patterns/strategy/TestStrategy.java
@@ -7,12 +7,14 @@ public class TestStrategy {
         Admin admin = new Admin();
         User user = new User();
 
+        System.out.println("Admin flow:");
         admin.makeSignUp();
         admin.makeSignOut();
         admin.makeSignIn();
 
         System.out.print("\n");
 
+        System.out.println("User flow:");
         user.makeSignUp();
         user.makeSignOut();
         user.makeSignIn();
