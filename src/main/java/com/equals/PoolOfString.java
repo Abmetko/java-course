5467a9216e76f60a36d3b008c0818431c0640b18
abmetko.a <abmetko.a@gmail.com>
2021-08-10 23:46:44 +0300

    refactoring

--- src/main/java/com/equals/PoolOfString.java
+++ src/main/java/com/equals/PoolOfString.java
@@ -7,6 +7,7 @@ public class PoolOfString {
     public static String text_3 = new String("test");
 
 
+    /** Сссылка на одну и ту же область в случае с text_1 и text_2 */
     public static void main(String[] args) {
         System.out.println(text_1 == text_2); //true
 
