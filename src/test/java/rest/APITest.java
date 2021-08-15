5e3fc4bd309c91bcc7d340059dddef6dd3118248
abmetko.a <abmetko.a@gmail.com>
2021-08-15 23:33:53 +0300

    refactoring

--- src/test/java/rest/APITest.java
+++ src/test/java/rest/APITest.java
@@ -4,13 +4,15 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import rest.enums.OrderTypes;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static rest.enums.Assets.ADAUSD;
+import static rest.enums.OrderTypes.CLOSED;
+import static rest.enums.OrderTypes.OPEN;
+
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class APITest {
+public class APITest extends APITestRunner {
 
     private static final RestService REST_SERVICE = new RestService();
     private static String token;
@@ -22,18 +24,19 @@ public class APITest {
 
     @Test
     public void symbolNameShouldBeEqualToExpectedValue() {
-        assertEquals(ADAUSD.name(), REST_SERVICE.getAssetLeverage(token).getSymbol());
+        assertEquals(ADAUSD.name(), REST_SERVICE.getAssetLeverage(token).getSymbol(),
+                "Asset name is incorrect");
     }
 
     @Test
     public void getClosedOrdersHistory() {
-        int numberOfClosedOrders = REST_SERVICE.getOrderHistory(token, ADAUSD.getAssetType(), OrderTypes.CLOSED.getType()).length;
-        assertEquals(0, numberOfClosedOrders);
+        int numberOfClosedOrders = REST_SERVICE.getOrderHistory(token, ADAUSD.getAssetType(), CLOSED.getType()).length;
+        assertEquals(0, numberOfClosedOrders, "Number of closed orders is incorrect");
     }
 
     @Test
     public void getOpenOrdersHistory() {
-        int numberOfOpenOrders = REST_SERVICE.getOrderHistory_2(token, ADAUSD.getAssetType(), OrderTypes.OPEN.getType()).length;
-        assertEquals(1, numberOfOpenOrders);
+        int numberOfOpenOrders = REST_SERVICE.getOrderHistory_2(token, ADAUSD.getAssetType(), OPEN.getType()).length;
+        assertEquals(1, numberOfOpenOrders, "Number of open orders is incorrect");
     }
 }
