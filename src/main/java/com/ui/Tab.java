87bcc56f4b79bb4443f1dc7c4c8a201f18623128
abmetko.a <abmetko.a@gmail.com>
2021-09-03 00:14:03 +0300

    refactoring

---
+++ src/main/java/com/ui/Tab.java
@@ -0,0 +1,34 @@
+package com.ui;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+
+import java.util.Objects;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class Tab {
+
+    public final SelenideElement COMPONENT_CONTAINER = $("div[role='tablist']");
+
+    public ElementsCollection getTabs() {
+        return COMPONENT_CONTAINER.$$("a[role='tab']");
+    }
+
+    public SelenideElement getTab(String name) {
+        return getTabs().stream()
+                .filter(menuItem -> menuItem.$("span.the-services__tab-text")
+                        .getText()
+                        .equalsIgnoreCase(name))
+                .findFirst().orElseThrow();
+    }
+
+    public void openTab(String name) {
+        getTab(name).shouldBe(Condition.enabled).shouldBe(Condition.visible).click();
+    }
+
+    public boolean isTabSelected(String name) {
+        return Objects.requireNonNull(getTab(name).getAttribute("class")).contains("active show");
+    }
+}
--- src/test/java/ui/junit5/ClientsTest.java
+++ src/test/java/ui/junit5/ClientsTest.java
@@ -1,14 +1,18 @@
 package ui.junit5;
 
 import com.ui.ContainerHeader;
+import com.ui.Tab;
 import com.ui.enums.MenuItems;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.junit.jupiter.api.parallel.ResourceLock;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.LocalDateTime;
 
@@ -21,7 +25,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class ClientsTest extends BaseTest {
 
     private final ContainerHeader containerHeader = new ContainerHeader();
+    private final Tab tab = new Tab();
 
+    @Order(1)
     @Test
     public void openClientsPage() {
         String name = MenuItems.CLIENTS.value;
@@ -30,10 +36,19 @@ public class ClientsTest extends BaseTest {
         System.out.println("open client page [" + this.getClass().getSimpleName() + "]: " + LocalDateTime.now());
     }
 
+    @Order(2)
     @Test
     public void openServicePage() {
         String name = MenuItems.SERVICE.value;
         containerHeader.selectHeaderMenu(name);
         assertTrue(containerHeader.isMenuItemSelected(name));
+    }
+
+    @Order(3)
+    @ParameterizedTest
+    @ValueSource(strings = { "Your Dedicated Team", "Web solutions", "Mobile Applications" })
+    public void openTab(String tabName) {
+        tab.openTab(tabName);
+        assertTrue(tab.isTabSelected(tabName));
     }
 }
