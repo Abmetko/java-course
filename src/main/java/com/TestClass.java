fd8587edc4efe9b7787e8763677d63af78adbe90
abmetko.a <abmetko.a@gmail.com>
2021-12-01 22:28:34 +0300

    refactoring

---
+++ src/main/java/com/TestClass.java
@@ -0,0 +1,14 @@
+package com;
+
+public class TestClass {
+
+    private String token;
+
+    public void print() {
+        System.out.println("Hello world!");
+    }
+
+    public static void print(String token) {
+        System.out.println("Hello world!");
+    }
+}
