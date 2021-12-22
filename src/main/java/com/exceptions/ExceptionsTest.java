e3370950e1dba610bd443196299b8f95fc2ed7d3
abmetko.a <abmetko.a@gmail.com>
2021-12-22 23:33:32 +0300

    implemented com.exceptions.NumberFoundException

--- src/main/java/com/exceptions/ExceptionsTest.java
+++ src/main/java/com/exceptions/ExceptionsTest.java
@@ -10,7 +10,17 @@ public class ExceptionsTest {
         }
     }
 
-    public static void main(String[] args) {
-        calculate(new int[]{1, 4, 5, 6, 7}, 3);
+    public static void checkNumbers(int number) throws NumberFoundException {
+        final int[] numbers = {0, 1, 2, 3, 4, 5};
+        for (int n : numbers) {
+            if (n == number) {
+                throw new NumberFoundException(String.format("Unexpectedly, a number [%d] has been found...", n));
+            }
+        }
+    }
+
+    public static void main(String[] args) throws NumberFoundException {
+//        calculate(new int[]{1, 4, 5, 6, 7}, 3);
+        checkNumbers(0);
     }
 }
