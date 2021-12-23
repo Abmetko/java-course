dd45efc30700063d087549b31d12898c15d22b0a
abmetko.a <abmetko.a@gmail.com>
2021-12-23 13:26:34 +0300

    refactoring

--- src/main/java/com/exceptions/ExceptionsTest.java
+++ src/main/java/com/exceptions/ExceptionsTest.java
@@ -10,20 +10,12 @@ public class ExceptionsTest {
         }
     }
 
-    public static void checkNumbers(int number) throws NumberFoundException {
-        final int[] numbers = {0, 1, 2, 3, 4, 5};
-        for (int n : numbers) {
-            if (n == number) {
-                throw new NumberFoundException(String.format("Unexpectedly, a number [%d] has been found...", n));
-            }
-        }
-    }
-
     public static void main(String[] args) throws NumberFoundException {
-//        calculate(new int[]{1, 4, 5, 6, 7}, 5);
-//        checkNumbers(0);
-
-        checkNumbers(0);
         calculate(new int[]{1, 4, 5, 6, 7}, 5);
+        System.out.println("\n");
+        NumberValidator.validateNumber(5);
+
+//        NumberValidator.validateNumber(0);
+//        calculate(new int[]{1, 4, 5, 6, 7}, 5);
     }
 }
