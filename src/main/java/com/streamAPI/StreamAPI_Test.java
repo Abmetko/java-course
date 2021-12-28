272ca2edb052e15ba2f72fda6a64675d5222e12e
abmetko.a <abmetko.a@gmail.com>
2021-12-28 21:20:50 +0300

    refactoring

--- src/main/java/com/streamAPI/StreamAPI_Test.java
+++ src/main/java/com/streamAPI/StreamAPI_Test.java
@@ -13,22 +13,34 @@ public class StreamAPI_Test {
     далее производится маппинг(преобразование каждого элемента коллекции) и цикл forEach
      */
     public static void streamMappingTest() {
-        list.stream().filter(i -> i % 2 != 0).collect(Collectors.toList()).stream().map(i -> i + 10).forEach(i -> {
-            System.out.printf("%d ", i);
-        });
+        list.stream()
+                .filter(i -> i % 2 != 0)
+                .collect(Collectors.toList())
+                .stream()
+                .map(i -> i + 10)
+                .forEach(i -> {
+                    System.out.printf("%d ", i);
+                });
     }
 
-    //пример, терминальный метод orElseThrow() и конвейерный filter(), findAny()
     public static void streamThrowExceptionTest(int val) {
-        list.stream().filter(i -> i.equals(val)).findAny()
+        list.stream().filter(i -> i.equals(val))
+                .findAny()
                 .orElseThrow(() -> new NullPointerException(String.format("there is no value: %d", val)));
         System.out.println("PASSED...");
     }
 
-    //после выполнения фильтра, проверка что только один элемент присутствует, иначе выбрасывается исключение
+    //после выполнения фильтра, проверка, что только один элемент присутствует, иначе выбрасывается исключение
     public static void streamReduceTest(int val) {
-        System.out.println("PASSED: " + list.stream().filter(i -> i.equals(val)).reduce((a, b) -> {
-            throw new IllegalStateException("FAILED: " + a + ", " + b);
-        }).isPresent());
+        System.out.println("PASSED: " + list.stream()
+                .filter(i -> i.equals(val))
+                .reduce((a, b) -> {
+                    throw new IllegalStateException("FAILED: " + a + ", " + b);
+                })
+                .isPresent());
+    }
+
+    public static void main(String[] args) {
+        streamReduceTest(10);
     }
 }
---
+++ src/main/java/com/streamAPI/StreamTest.java
@@ -0,0 +1,27 @@
+package com.streamAPI;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+@SuppressWarnings("ALL")
+public class StreamTest {
+
+    private static final List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+    public static void main(String[] args) {
+        //реализация базовая
+        Iterator<Integer> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        //реализация итератора с помощью продвинутого цикла for
+        for (Integer i : list) {
+            System.out.println(i);
+        }
+
+        //реализация с помощью Stream API - любую коллекцию, имплементирующую интерфейс Collection
+        list.stream().forEach(i -> System.out.println(i));
+    }
+}
