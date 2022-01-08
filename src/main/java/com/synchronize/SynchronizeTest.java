a9a21ab5d5a1e8db565fd2f927cd98df9895b74b
abmetko.a <abmetko.a@gmail.com>
2022-01-09 00:24:24 +0300

    refactoring

--- src/main/java/com/synchronize/SynchronizeTest.java
+++ src/main/java/com/synchronize/SynchronizeTest.java
@@ -36,6 +36,14 @@ public class SynchronizeTest {
         Thread thread_5 = new Thread(() -> setCurrentTime());
         Thread thread_6 = new Thread(() -> setCurrentTime());
 
+        //старый способ с анонимным классом
+        Thread thread_7 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                setCurrentTime();
+            }
+        });
+
         thread_1.start();
         thread_2.start();
         thread_3.start();
