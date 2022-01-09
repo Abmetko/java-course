f7b5a11f0a3a5e64e47a28bd85ffcf4b79d8ee1a
abmetko.a <abmetko.a@gmail.com>
2022-01-09 18:00:15 +0300

    refactoring

--- src/main/java/com/collections/ArrayListTest.java
+++ src/main/java/com/collections/ArrayListTest.java
@@ -6,7 +6,6 @@ import java.util.List;
 public class ArrayListTest {
 
     public static void main(String[] args) {
-
         List<String> list = new ArrayList<>();
         list.add("Stasia");
         list.add("Andrey");
